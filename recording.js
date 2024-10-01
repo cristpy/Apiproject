274ce854ec0c073d4718911067c6ae3eedbeab02
cristpy <cortezchristian212@gmail.com>
2024-10-01 09:31:01 +0000

    commit

---
+++ recording.js
@@ -0,0 +1,33 @@
+let mediaRecorder;
+let audioChunks = [];
+
+navigator.mediaDevices.getUserMedia({ audio: true})
+    .then(stream => {
+        mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.ondataavailable = event => {
+            audioChunks.push(event.data);
+        };
+
+        mediaRecorder.onstop = () => {
+            const audioBlob = new Blob(audioChunks, { type: `audio/wav`});
+            const audioUrl = URL.createObjectURL(audioBlob);
+            const audio = new Audio(audioUrl);
+            audio.play();
+
+            sendAudioToBackend(audioBlob);
+        };
+    });
+
+function startRecording() {
+    audioChunks = [];
+    mediaRecorder.start();
+}
+
+function stopRecordig() {
+    mediaRecorder.stop();
+}
+
+function sendAudioToBackend(audioBlob) {
+    const formData = new FormData();
+    formData.append(`audio`, audioBlob, `recording.wav`);
+}
