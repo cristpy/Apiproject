b260f6eed17d7149f36d19029f6dd221f4a35bee
cristpy <cortezchristian212@gmail.com>
2024-10-24 16:50:19 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // client/src/App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import LandingPage from './component/LandingPage';
 import UploadPage from './component/UploadPage';
 import ResultPage from './component/ResultPage';
@@ -8,15 +9,56 @@ import './styles/LandingPage.scss';
 import './styles/LoadingSpinner.scss';
 import './styles/ResultPage.scss';
 import './styles/UploadPage.scss';
-const App: React.FC = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/upload" element={<UploadPage />} />
-      <Route path="/result" element={<ResultPage />} />
-    </Routes>
-  </Router>
-);
+import './styles/global.scss';
+
+const App: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -50 }}
+              transition={{ duration: 0.5 }}
+            >
+              <LandingPage />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/upload"
+          element={
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -50 }}
+              transition={{ duration: 0.5 }}
+            >
+              <UploadPage />
+            </motion.div>
+          }
+        />
+        <Route
+          path="/result"
+          element={
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -50 }}
+              transition={{ duration: 0.5 }}
+            >
+              <ResultPage />
+            </motion.div>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 export default App;
-
